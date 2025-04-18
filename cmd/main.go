27896817f7cd87bad8aed1c4870fddac2510ec8a
golovanevvs <lony@inbox.ru>
2025-04-18 16:41:03 +0300

    task21

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
+import "ozon-test2/internal/task2/task21"
+
 func main() {
 	// task11.Task11()
-	task21.Task21()
+	task21.Task()
 }
--- internal/task2/task21/task21.go
+++ internal/task2/task21/task21.go
@@ -4,13 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+type bank struct {
+	rd float64
+	re float64
+	dr float64
+	de float64
+	er float64
+	ed float64
+}
 
 func Task() {
 	// Раскомментить при запуске на своей машине, закомментить при отправке на платформу
 	// В папку tests скопировать тесты с платформы
 	// Использовать для тестирования на своей машине, используя данные из указанного файла
-	file, err := os.Open("../internal/task1/tests/2")
+	file, err := os.Open("../internal/task2/tests/1")
 	if err != nil {
 		fmt.Printf("Ошибка открытия файла: %s\n", err.Error())
 	}
@@ -36,51 +48,88 @@ func Run(in *bufio.Reader, out *bufio.Writer) {
 	// Решение t подзадач
 	for i := 0; i < t; i++ {
 		// Примеры чтения параметров подзадачи i
+		banks := make([]bank, 3)
 
-		// Чтение из одной строки нескольких значений, разделённых пробелом
-		var s string
-		fmt.Fscanln(in, &s)
+		for j := 0; j < 3; j++ {
+			for k := 0; k < 6; k++ {
 
-		// Чтение целой строки
-		// str1, _ := in.ReadString('\n')
-		// Удаление символа \n (на некоторых машинах надо удалить два символа \r\n)
-		// str := strings.Trim(str1, "\n")
-		// str := strings.Trim(str1, "\r\n")
-		// Для избежания ошибки выведем str (закомментить при использовании шаблона)
-		// fmt.Println(str)
+				// Чтение из одной строки нескольких значений, разделённых пробелом
+				// var s string
+				// fmt.Fscan(in, &s)
+				// fmt.Println(s)
 
-		// Запуск и вывод в out решения подзадачи t
-		// В зависимости от условия задачи алгоритм вывода может потребовать доработки
-		fmt.Fprintln(out, tTaskSolving(s))
+				// Чтение целой строки до переноса
+				str1, _ := in.ReadString('\n')
+				// Удаление символа \n (на некоторых машинах надо удалить два символа \r\n)
+				str := strings.Trim(str1, "\n")
+				// str := strings.Trim(str1, "\r\n")
+				// Для избежания ошибки выведем str (закомментить при использовании шаблона)
+				valueStrSlice := strings.Split(str, " ")
+				value1, _ := strconv.ParseFloat(valueStrSlice[0], 64)
+				value2, _ := strconv.ParseFloat(valueStrSlice[1], 64)
+				value := value2 / value1
+				switch k {
+				case 0:
+					banks[j].rd = value
+				case 1:
+					banks[j].re = value
+				case 2:
+					banks[j].dr = value
+				case 3:
+					banks[j].de = value
+				case 4:
+					banks[j].er = value
+				case 5:
+					banks[j].ed = value
+				}
+
+				// Запуск и вывод в out решения подзадачи t
+				// В зависимости от условия задачи алгоритм вывода может потребовать доработки
+			}
+		}
+		fmt.Fprintln(out, tTaskSolving(banks))
 	}
 }
 
 // tTaskSolving## - функция для решения подзадачи t задачи # (вариант #)
 // В зависимости от условия задачи, необходимо указать требуемые аргументы и возвращаемое значение функции
-func tTaskSolving(s string) string {
-	if len(s) == 1 {
-		return "YES"
-	}
-
-	if s[0] != s[len(s)-1] {
-		return "NO"
-	}
-
-	if len(s) < 3 && s[0] == s[1] {
-		return "YES"
-	}
-
-	for i := 0; i <= len(s)-3; {
-		if s[i] == s[i+1] {
-			i++
-		} else {
-			if s[i] != s[i+1] && s[i] != s[i+2] {
-				return "NO"
-			} else if s[i] != s[i+1] && s[i] == s[i+2] {
-				i += 2
-			}
+func tTaskSolving(banks []bank) (maxD float64) {
+	maxDs := make([]float64, 0)
+	for i := range banks {
+		var a0, a1, a2 int
+		switch i {
+		case 0:
+			a0 = 0
+			a1 = 1
+			a2 = 2
+		case 1:
+			a1 = 0
+			a0 = 1
+			a2 = 2
+		case 2:
+			a2 = 0
+			a1 = 1
+			a0 = 2
 		}
+		d1 := banks[a2].rd + banks[a1].dr + banks[a0].rd
+		maxDs = append(maxDs, d1)
+		d2 := banks[a2].rd + banks[a1].er + banks[a0].rd
+		maxDs = append(maxDs, d2)
+		d3 := banks[a2].rd + banks[a1].de + banks[a0].ed
+		maxDs = append(maxDs, d3)
+		d4 := banks[a1].rd + banks[a2].dr + banks[a0].rd
+		maxDs = append(maxDs, d4)
+		d5 := banks[a1].rd + banks[a2].er + banks[a0].rd
+		maxDs = append(maxDs, d5)
+		d6 := banks[a1].rd + banks[a2].de + banks[a0].ed
+		maxDs = append(maxDs, d6)
+		d7 := banks[a1].re + banks[a0].ed
+		maxDs = append(maxDs, d7)
+		d8 := banks[a2].re + banks[a0].ed
+		maxDs = append(maxDs, d8)
+		d9 := banks[a0].rd
+		maxDs = append(maxDs, d9)
 	}
 
-	return "YES"
+	return slices.Max(maxDs)
 }
