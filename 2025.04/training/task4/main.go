17b1cee4610411388333400723dc4263aa7c0ab4
golovanevvs <lony@inbox.ru>
2025-05-30 19:58:27 +0300

    task4: main

--- 2025.04/training/task4/main.go
+++ 2025.04/training/task4/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/golovanevvs/ozon-route-256-contest-go/2025.04/training/task4/task43"
+	"github.com/golovanevvs/ozon-route-256-contest-go/2025.04/training/task4/task44"
 )
 
 func main() {
-	task43.Task()
+	task44.Task()
 }
